4ff21d8bcf18d0ab7c6ea79fa0caf0e96bc9be8c
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-11-28 11:51:38 +0900

    first commit
    最初のアプリの内容をローカルリポジトリに入れた

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+# postモデルに対応するpostsテーブルに対してallメソッドを使うことでpostsテーブルにあるレコードを全て取得できる
+# 取得した複数のレコードを@postsというインスタンス変数に代入することでposts/index.html.erbファイルで@postsを使うことができる
+
+  def new
+  end
+# newアクションに対応するnew.html.erbは新規投稿ページである
+# 新規投稿ページではpostsテーブルに保存されているデータを取得する必要が無いのでアクション内に処理を記述する必要がない
+
+  def create
+    Post.create(content: params[:content])
+  end
+# postモデルに対応するpostsテーブルに投稿内容を保存したいのでpostモデル(大文字から始める)にテーブルに保存するためのcreateメソッドを使っている
+# どこに保存するのかはcreateメソッドの引数の左側に記述する 今回はpostsテーブルのcontentカラムに保存するということになる
+# 何を保存するのかはcreateメソッドの引数の右側に記述する 投稿フォームのコードにはform.text_field :content とある
+# 投稿内容を記入して投稿するボタンを押すとキーがcontentバリューが投稿内容であるparamsというハッシュが送られる
+# そのハッシュから投稿内容を取り出すためにparams[:content]としている
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get "/", to: "posts#index"
+  get "/posts/new", to: "posts#new"
+  post "/posts", to: "posts#create"
+end
+
+# /というURLでリクエストを送るとそのリクエストはルーティングに進みそのURLにあったpostsコントローラーのindexアクションに進ませる
+# indexアクションに対応したposts/index.html.erbファイルがブラウザに表示される
+
+
+# 新規投稿ページのHTMLを取得したいのでHTTP通信(クライアントとサーバーの間でデータを送受信するための通信)で使われるHTTPメソッドはGETになる
+# 新規投稿ページに行くために/posts/newというパスでリクエストを送るとpostsコントローラーのnewアクションに繋がる
+
+# /postsというURLで投稿内容を持ったリクエストを送るとそのリクエストはまずルーティングにくる
+# これから投稿内容が送信されようとしているのでHTTPメソッドはpostにする必要がある
+# URLに関しては一覧表示と被っているがHTTPメソッドが異なっているので気にすることはない
+# /postsというURLでリクエストが来たのでそのURLに対応するpostsコントローラーのcreateアクションが動く
